dc9f1d16448f4fe4163b8df68532792af2dc9ba7
matthewjholmes <ilookup@gmail.com>
2021-12-08 11:13:51 -0700

    item index

---
+++ spec/requests/api/v1/items/index_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe 'Items index endpoint' do
+  it 'returns all items' do
+    merchant = create(:merchant)
+    create_list(:item, 5, merchant_id: merchant.id)
+
+    get '/api/v1/items'
+
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body, symbolize_names: true)
+
+    expect(items).to be_a(Hash)
+    expect(items[:data].count).to eq(5)
+
+    items[:data].each do |item|
+      expect(item).to have_key(:id)
+      expect(item[:id]).to be_a(String)
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes][:description]).to be_a(String)
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes][:unit_price]).to be_a(Float)
+    end
+  end
+
+  it 'returns an empty array for no items' do
+    get '/api/v1/items'
+
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body, symbolize_names: true)
+
+    expect(items).to have_key(:data)
+    expect(items[:data]).to be_an(Array)
+    expect(items[:data]).to eq([])
+  end
+  #sad path
+end
